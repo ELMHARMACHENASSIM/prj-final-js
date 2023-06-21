388f1f9de8c27cb67ae3e4f6dc4a5d9f8b4f303f
NASSIM ELM'HARMACHE <48417897+ELMHARMACHENASSIM@users.noreply.github.com>
2023-06-21 22:00:34 +0100

    add two  section

--- description.md
+++ description.md
@@ -1,21 +1,15 @@
 
-   
- 
 
 
-ENJOY YOUR HEALTHY DELICIOUS FOOD
-Sed autem laudantium dolores. Voluptatem itaque ea consequatur
-eveniet. Eum quas beatae cumque eum quaerat.
-Book a Table
-Watch Video
-Book a Table
-+1 5589 55488 55
-ABOUT US
-LEARN MORE ABOUT US
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ullamco laboris nisi ut aliquip ex ea commodo consequat. ✓ Duis aute irure dolor in reprehenderit in voluptate velit.
-Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.
-Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+
+
+
+
+
+
+
+
+
 Why Choose LionsResto?
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
 Corporis voluptates officia eiusmod
--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,39 @@
-let dvmMobile =  document.querySelector(".divmobile");
-let openNav =  document.querySelector("#open");
-let closeNav =  document.querySelector("#close");
-console.log(dvmMobile,closeNav,openNav);
 
-openNav.addEventListener("click",()=>{
-    dvmMobile.style.right = "0"
+
+
+let dvmMobile = document.querySelector(".divmobile");
+let openNav = document.querySelector("#open");
+let closeNav = document.querySelector("#close");
+
+openNav.addEventListener("click", () => {
+  dvmMobile.style.right = "0";
 });
 
-closeNav.addEventListener("click",()=>{
-    dvmMobile.style.right = "-100"+ "%"
+closeNav.addEventListener("click", () => {
+  dvmMobile.style.right = "-100" + "%";
 });
+
+// ---------------------------------------------------------
+let watchVd = document.querySelector(".watch");
+let closeVd = document.querySelector("#close-vd");
+let vdContent = document.querySelector(".fixed-vd");
+let frameVd = document.querySelector(".frame-vd")
+console.log(watchVd, closeVd, vdContent);
+
+function openVd() {
+  vdContent.classList.add("show");
+ 
+}
+
+function closeVideo() {
+  vdContent.classList.remove("show");
+  frameVd.pause();
+}
+ watchVd.addEventListener("click", () => {
+  openVd();
+});
+closeVd.addEventListener("click", () => {
+  closeVideo();
+
+}); 
+
