fc21e98939a508597bf51de9735c318bd335208a
NASSIM ELM'HARMACHE <48417897+ELMHARMACHENASSIM@users.noreply.github.com>
2023-06-23 15:14:49 +0100

    add up btn

--- public/js/app.js
+++ public/js/app.js
@@ -169,3 +169,20 @@ const dragStopImg = (ev) => {
 caruselImg.addEventListener("mouseover", dragaImg);
 caruselImg.addEventListener("mousedown", dragStartImg);
 document.addEventListener("mouseup", dragStopImg);
+
+// --------------------------------------------------------
+let scroll_up = document.querySelector(".up")
+window.onscroll = function () {
+    if (this.scrollY >= 500) {
+        scroll_up.classList.add("show")
+    } else {
+        scroll_up.classList.remove("show")
+
+    }
+}
+scroll_up.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+})
